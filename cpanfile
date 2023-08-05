fe0eb03020b49b05dbc9888094434aafe6d8352e
Erik Huelsmann <ehuels@gmail.com>
2023-08-05 12:25:09 +0200

    Add tests and release infrastructure

--- cpanfile
+++ cpanfile
@@ -1,3 +1,8 @@
 
 requires 'perl' => '5.14.0';
+requires 'Authen::SCRAM' => '0.011';
+requires 'Feature::Compat::Try' => '0.04';
 
+on develop => sub {
+    requires 'Test2::V0';
+};
--- lib/Authen/SASL/SCRAM.pm
+++ lib/Authen/SASL/SCRAM.pm
@@ -6,12 +6,71 @@ use Feature::Compat::Try;
 
 package Authen::SASL::SCRAM;
 
+=head1 NAME
+
+Authen::SASL::SCRAM - SCRAM support for Authen::SASL
+
+=head1 VERSION
+
+0.01
+
+=head1 SYNOPSIS
+
+   # with Authen::SASL::SCRAM installed
+   use Authen::SASL;
+
+   my $client = Authen::SASL->new(
+        username => 'user',
+        password => 'pass',
+        mechanism => 'SCRAM-SHA-512 SCRAM-SHA-256 SCRAM-SHA-1 PLAIN'
+   );
+   # authenticates using SCRAM SHA hash or PLAIN
+
+
+   my $salt = 'your-precious-salt';
+   # $server_key and $stored_key need to be looked up from a user store
+   my $server_key = 'server-key-stored-for-this-user';
+   my $stored_key = 'key-stored-for-this-user';
+   my $server => Authen::SASL->new(
+       mechanism => 'SCRAM-SHA-1', # selected mechanism
+       callback => {
+            getsecret => sub {
+                 my $username = shift;
+                 return ($salt, $stored_key, $server_key, $iterations);
+            },
+       }
+   );
+
+
+=head1 DESCRIPTION
+
+The C<Authen::SASL::SCRAM> distribution adds L<Authen::SASL> support for
+SCRAM authentication using the mechanisms listed below by wrapping
+L<Authen::SCRAM>.
+
+=over
+
+=item SHA-1 (SCRAM-SHA-1)
+
+=item SHA-256 (SCRAM-SHA-256)
+
+=item SHA-512 (SCRAM-SHA-512)
+
+=back
+
+The *-PLUS variants are not supported at this time.
+
+=cut
+
 use Authen::SASL;
 
 use parent qw(Authen::SASL::Perl);
 
 use Authen::SCRAM::Client;
 use Authen::SCRAM::Server;
+
+
+our @VERSION = '0.01';
 
 my %secflags = (
   noplaintext => 1,
@@ -109,6 +168,25 @@ sub server_step {
     }
 }
 
+=head1 BUGS
 
+Please report bugs via
+L<https://github.com/ehuelsmann/authen-sasl-scram/issues>.
+
+=head1 SEE ALSO
+
+L<Authen::SASL>, L<Authen::SCRAM>
+
+=head1 AUTHOR
+
+Erik Huelsmann <ehuels@gmail.com>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2023 Erik Huelsmann. All rights reserved. This program is
+free software; you can redistribute it and/or modify it under the same
+terms as Perl itself.
+
+=cut
 
 1;
